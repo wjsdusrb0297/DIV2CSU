3a6db7d1ff69ea68c22da47c47a54451b870df85
Jay Cho <jaycho1214@gmail.com>
2023-09-25 19:04:24 +0900

    remove unnecessary fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import { api } from '@/lib/instance';
-import { MenuCard } from './components';
-
 export default async function Home() {
-  const data = await api.get('/soldiers').json();
   return <div></div>;
 }
