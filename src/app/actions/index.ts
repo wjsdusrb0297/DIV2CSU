909d116eab4919192279b03f3ecafd579b0df816
Jay Cho <jaycho1214@gmail.com>
2023-10-18 22:47:53 +0900

    add import

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,2 +1,4 @@
 export * from './auth';
 export * from './soldiers';
+export * from './permissions';
+export * from './points';
--- src/app/soldiers/components/index.ts
+++ src/app/soldiers/components/index.ts
@@ -1,3 +1,4 @@
 export * from './HelpModal';
+export * from './PasswordForm';
 export * from './PasswordModal';
 export * from './PermissionsTransfer';
