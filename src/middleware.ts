c59fddcf4ae9127a1b8e73399a6d2d2a8b89d828
Jay Cho <jaycho1214@gmail.com>
2023-10-22 12:37:13 +0900

    handle auth redirect on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,4 +8,24 @@ export async function middleware(request: NextRequest) {
       },
     });
   }
+  if (
+    !request.cookies.get('auth.access_token') &&
+    !request.nextUrl.pathname.startsWith('/auth')
+  ) {
+    return NextResponse.redirect(new URL('/auth/login', request.url));
+  }
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - robots.txt (robots file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt).*)',
+  ],
+};
