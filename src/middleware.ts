86ff8a5704d904b4326426bbd198d1f394d9ecfd
Jay Cho <jaycho1214@gmail.com>
2023-10-22 12:19:07 +0900

    use URL class for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/auth/logout') {
-    return NextResponse.redirect(`${request.nextUrl.origin}/auth/login`, {
+    return NextResponse.redirect(new URL('/auth/login', request.url), {
       headers: {
         'Set-Cookie': 'auth.access_token=deleted; Path=/; Max-Age=-1',
       },
